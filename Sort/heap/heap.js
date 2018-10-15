e4f8267aa79ad8fd3c21f8e92976b8902bf4d3a9
TPQuietBro <1186886308@qq.com>
2018-10-16 00:06:44 +0800

    update heap

--- Sort/heap/heap.js
+++ Sort/heap/heap.js
@@ -1,3 +1,43 @@
-
+/**
+ 思路:
+ 1. 构造一个大顶堆(或小顶堆).
+ 2. 交换根结点和最后一个叶子结点,然后删除交换后的叶子结点
+ 3. 此时形成了一个新的二叉树,重复步骤1直到最后排序完成.
+*/
 var arr = new Array(49,38,65,97,76,13,27,49,55,04);
-// 等待分析
+
+function adjustHeap(arr,i,length) {
+    var left;
+    var right;
+    var j;
+    var temp = arr[i];
+    while ((left = 2 * i + 1) <= length) {
+        right = left + 1;
+        j = left; // 保存左结点
+        if (j < length && arr[left] < arr[right]) {
+            j++;
+        }
+        if (arr[i] < arr[j]){
+            swap(arr,i,j);
+        } else {
+            break;
+        }
+        i = j;
+    }
+}
+
+function heapSort(arr) {
+    var length = arr.length - 1;
+    for (let i = length/2 - 1; i >= 0; i--){
+        adjustHeap(arr,i,arr.length);
+    }
+    while (length >= 0) {
+        swap(arr,0,length--);
+        adjustHeap(arr,0,length);
+        console.log(arr);
+    }
+}
+
+heapSort(arr);
+
+alert(arr);
